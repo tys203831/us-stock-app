7ee5e001dc8bb09cb8947306395082777074feb6
tys203831 <tys203831@gmail.com>
2022-04-15 11:24:12 +0800

    second try

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('Uber pickups in NYC')
+st.set_page_config(page_title="My Website", page_icon=":wave:", layout="wide")
+st.subheader('Hi, I am Yong Sheng :smile:')
+
+st.title("A first try on streamlit website building")
+
+st.write("I am passionate to write python code to get more data.")
+st.write("[Learn more here:](https://tanyongsheng.xyz)")
