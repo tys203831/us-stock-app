73d80efb4ac07594f044cd1010bb67fb4c8b47a3
tys203831 <tys203831@gmail.com>
2022-04-15 11:50:50 +0800

    third try

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
+import requests
 
 st.set_page_config(page_title="My Website", page_icon=":wave:", layout="wide")
-
-
 
 # --HEADER SECTION---
 with st.container():
@@ -11,10 +10,21 @@ with st.container():
     st.write("I am passionate to write python code to get more data.")
     st.write("[Learn more here:](https://tanyongsheng.xyz)")
 
-# ---- ------
+# --- LOAD ASSET ---
+lottie_coding = "https://assets8.lottiefiles.com/packages/lf20_tno6cg2w.json"
+
+def load_lottieurl(url):
+    image = requests.get(lottie_coding)
+    if image.status_code !=200:
+        return None
+    return image.json()
+
+# ---- BODY SECTION ------
 with st.container():
     st.write("----")
+    #--- CREATE 2 COLUMNS ----
     left_column, right_column = st.columns(2)
+    #--- ADD CONTENT INTO LEFT COLUMN ---  
     with left_column:
         st.write("##")
         st.write("""
@@ -22,5 +32,8 @@ with st.container():
                 I could complete all my tasks through continuing my efforts to do something that I like.
                 """)
         st.write("[Affiliate website >](https:pickufly.com)")
-    
-    
+    # 
+
+    with right_column:
+        st.write(load_lottieurl(lottie_coding))
+        
