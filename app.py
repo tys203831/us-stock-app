b73478a7620e75a3c75065561e054a83292a9a86
tys203831 <tys203831@gmail.com>
2022-04-15 13:09:53 +0800

    try

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 from streamlit_lottie import st_lottie
+from PIL import Image
 
 st.set_page_config(page_title="My Website", page_icon=":wave:", layout="wide")
 
@@ -12,6 +13,7 @@ with st.container():
     st.write("[Learn more here:](https://tanyongsheng.xyz)")
 
 # --- LOAD ASSET ---
+## --- LOAD ANIMATION ---
 def load_lottieurl(url):
     image = requests.get(url)
     if image.status_code !=200:
@@ -20,6 +22,9 @@ def load_lottieurl(url):
 
 lottie_coding = load_lottieurl("https://assets8.lottiefiles.com/packages/lf20_tno6cg2w.json")
 
+# -- LOAD IMAGES --
+bird_image = Image.open("./images/bird.jpg")
+
 # ---- BODY SECTION ------
 with st.container():
     st.write("----")
@@ -27,7 +32,7 @@ with st.container():
     left_column, right_column = st.columns(2)
     #--- ADD CONTENT INTO LEFT COLUMN ---  
     with left_column:
-        st.write("##")
+        #st.write("##")
         st.write("""
                 I am trying to learn as much as possible, hoping that I would be able to integrate my skills.
                 I could complete all my tasks through continuing my efforts to do something that I like.
@@ -37,3 +42,17 @@ with st.container():
     # ADD ANIMATION to the right column
     with right_column:
         st_lottie(lottie_coding, height=300, key="coding")
+
+# Projects
+with st.container():
+    st.header("Projects")
+    with st.container():
+        image_column, text_column = st.columns((1,2))
+        with image_column:
+            # insert image
+            st.image(bird_image)
+        with text_column:
+            st.subheader("this is a test")
+            st.write("""
+            This is my first test to display my projects.
+            """)
